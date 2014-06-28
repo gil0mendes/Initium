01f226c71d29bb16fbc2f2aacb43ee42ccd3e6f1
gil0mendes <gil00mendes@gmail.com>
2014-06-28 19:37:13 +0100

    EFI: Add more API definitions
    Add definitions for the system table, boot/runtime services tables, and
    the simple text input/output protocols. Add the -fshort-wchar option to
    CCFLAGS which allows us to pass L"..." string literals to EFI, as EFI
    uses UCS-2 strings. We use the __attribute__((ms_abi)) attribute on EFI
    API functions on x86_64 to use the correct calling convention (EFI uses
    the MS x86_64 calling convention). This requires a recent compiler (GCC
    4.7 or later, clang 3.4 or later).

---
+++ source/loader/platform/efi/arch/amd64/include/efi/arch/api.h
@@ -0,0 +1,42 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Gil Mendes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+ /**
+ * @file
+ * @brief		AMD64 EFI API definitions.
+ */
+
+#ifndef __EFI_ARCH_API_H
+#define __EFI_ARCH_API_H
+
+/**
+ * EFI calling convention attribute.
+ *
+ * On x86_64, EFI uses the Microsoft calling convention, therefore we must
+ * mark all EFI APIs with the ms_abi attribute so that the right calling
+ * convention is used.
+ */
+#define __eficall	__attribute__((ms_abi))
+
+#endif /* __EFI_ARCH_API_H */
