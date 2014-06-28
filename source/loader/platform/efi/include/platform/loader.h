0444a3fdad90f4b49af8783f735daa74ba586e2c
gil0mendes <gil00mendes@gmail.com>
2014-06-28 18:25:49 +0100

    Add initial UEFI platform code (currently 64-bit x86 only)
    Adds the initial build infrastructure for UEFI platforms, and an
    efi-amd64 configuration to build the loader to run under UFEI on 64-bit
    x86 systems. Since UEFI supports multiple architectures, the UEFI
    platform is split into generic and architecture dependent code.

---
+++ source/loader/platform/efi/include/platform/loader.h
@@ -0,0 +1,46 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Gil Mendes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+* @file
+* @brief		EFI platform core definitions.
+*/
+
+#ifndef __PLATFORM_LOADER_H
+#define __PLATFORM_LOADER_H
+
+// Segment defintions.
+#define SEGMENT_CS		  0x08		// Code segment
+#define SEGMENT_DS		  0x10		// Data segment
+#define SEGMENT_CS16		0x18		// 16-bit code segment
+#define SEGMENT_DS16		0x20		// 16-bit code segment
+#define SEGMENT_CS64		0x28		// 64-bit code segment
+#define SEGMENT_DS64		0x30		// 64-bit data segment
+
+#ifndef __ASM__
+
+extern void platform_init(void);
+
+#endif /* __ASM__ */
+#endif /* __PLATFORM_LOADER_H */
