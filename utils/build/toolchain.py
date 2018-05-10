f35edf5ae547cc544a69dcbfbb5267ddcdf9317b
Gil Mendes <gil00mendes@gmail.com>
2018-05-10 09:29:25 +0100

    Implement code to download and unpack packages

--- utils/build/toolchain.py
+++ utils/build/toolchain.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+from subprocess import Popen, PIPE
+from time import time
+from urllib.parse import urlparse
 
 def msg(msg):
     """ Print out a message to the console """
@@ -8,6 +11,34 @@ def msg(msg):
 class ToolchainComponent:
     def __init__(self, manager):
         self.manager = manager
+        self.destdir = manager.generic_dir
+
+    def check(self):
+        """ Check if the component requires updating. """
+        path = os.path.join(self.destdir, f'.{self.name}-{self.version}-installed')
+        if not os.path.exists(path):
+            return True
+
+        return False
+
+    def download(self):
+        """ Download and unpack all sources for the component. """
+        for url in self.sources:
+            name = urlparse(url).path.split('/')[-1]
+            target = os.path(self.manager.destdir, name)
+            if not os.path.exists(target):
+                msg(f' Downloading source file: {name}')
+
+                # Download to .part and then rename when complete so we can
+                # easily do continuing of downloads.
+                self.execute(f'wget -c -O {target}.part {url}')
+                os.rename(target + '.part', target)
+
+            # Unpack if this is a tarball.
+            if name[-8:] == '.tar.bz2':
+                self.execute(f'tar -C {self.manager.build_dir} -xjf {target}')
+            elif name[-7:] == '.tar.gz':
+                self.execute(f'tar -C {self.manager.build_dir} -xzf {target}')
 
 class BinutilsComponent(ToolchainComponent):
     name = 'binutils'
