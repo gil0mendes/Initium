0f12ccbc3ce93f6e19492784c6968f5138937fd5
Gil Mendes <gil00mendes@gmail.com>
2018-05-12 13:05:45 +0100

    Rearrange folders

--- platform/efi/src/lib.rs
+++ platform/efi/src/lib.rs
@@ -4,39 +4,37 @@
 #![no_std]
 #![no_main]
 
-#[macro_use]
-extern crate fixedvec;
+extern crate libuefi;
 
-#[macro_use]
-mod print;
-mod uefi;
+use libuefi::system_table::SystemTable;
 
-pub(crate) static mut UEFI_SYSTEM_TABLE: Option<&'static uefi::SystemTable> = None;
+// pub(crate) static mut UEFI_SYSTEM_TABLE: Option<&'static uefi::SystemTable> = None;
 
 /// Entry point for EFI platforms
 #[no_mangle]
-pub extern "win64" fn uefi_start(handle: uefi::Handle, system_table: &'static uefi::SystemTable) -> uefi::Status {
-    unsafe {
-        UEFI_SYSTEM_TABLE = Some(&system_table);
-    }
-    println!("UEFI header: {:#?}", system_table.get_header());
-    main();
-    uefi::Status::Success
+pub extern "win64" fn uefi_start(image_handle: usize, system_table: SystemTable) {
+    loop{}
+    // unsafe {
+    //     UEFI_SYSTEM_TABLE = Some(&system_table);
+    // }
+    // println!("UEFI header: {:#?}", system_table.get_header());
+    // main();
+    // uefi::Status::Success
 }
 
-fn main() {
-    println!("Hello, {}!", "UEFI world");
-    println!("Version: {}", env!("CARGO_PKG_VERSION"));
-    println!("Authors:");
-    for author in env!("CARGO_PKG_AUTHORS").split(';') {
-        println!("    {}", author);
-    }
-}
+// fn main() {
+//     println!("Hello, {}!", "UEFI world");
+//     println!("Version: {}", env!("CARGO_PKG_VERSION"));
+//     println!("Authors:");
+//     for author in env!("CARGO_PKG_AUTHORS").split(';') {
+//         println!("    {}", author);
+//     }
+// }
 
 #[lang = "panic_fmt"]
 #[no_mangle]
 pub extern fn rust_begin_panic(message: core::fmt::Arguments, file: &'static str, line: u32, column: u32) -> ! {
-    println!("Panic in {file} at {line}:{column}: {message}", message=message, file=file, line=line, column=column);
+    // println!("Panic in {file} at {line}:{column}: {message}", message=message, file=file, line=line, column=column);
     loop {}
 }
 
