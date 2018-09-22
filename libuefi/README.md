6f54d52e1fb5e4436bd5bf0ca82c7886cd7f97dd
Gil Mendes <gil00mendes@gmail.com>
2018-09-22 10:19:23 +0100

    Remove old libuefi
    Now we use uefi-rs lib from @GabrielMajeri

--- libuefi/README.md
+++
@@ -1,3 +0,0 @@
-# UEFI Types
-
-This crate is for supporting UEFI applications such as bootloader and operating systems.
--- libuefi/src/protocols/console/text/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Simple Text I/O.
-mod output;
-mod input;
-
-pub use self::output::Output;
-pub use self::input::Input;
