795e2ee756a1840781b4953fed55bc302d687c5f
Gil Mendesq <gil00mendes@gmail.com>
2021-01-23 15:40:36 +0000

    feat: reset console before start shell

--- src/console.rs
+++ src/console.rs
@@ -1,4 +1,4 @@
-use common::console::{ConsoleIn, ConsoleOut};
+use common::console::{Color, ConsoleIn, ConsoleOut};
 
 /// Get borrow reference for console output
 pub fn get_console_out<'a>() -> &'a mut dyn ConsoleOut {
@@ -18,4 +18,16 @@ pub fn get_console_in<'a>() -> &'a mut dyn ConsoleIn {
             .as_mut()
             .expect("Console does not support input capability")
     }
+}
+
+/// End UI mode reset the console.
+pub fn console_end_ui() {
+    // TODO: check if we are on UI mode
+    let mut console = get_console_out();
+
+    // reset state and clear the default colors
+    console.reset_region();
+    console.set_cursor(0, 0, true);
+    console.set_color(Color::White, Color::Black);
+    console.clear(0, 0, 0, 0);
 }
