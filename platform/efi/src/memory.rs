e061891c7bc027d804ec40fc5d22619333f54996
Gil Mendesq <gil00mendes@gmail.com>
2021-01-24 13:56:00 +0000

    feat: first steps for having buildtin commands

--- platform/efi/src/memory.rs
+++ platform/efi/src/memory.rs
@@ -1,11 +1,9 @@
 //! EFI memory allocation manager
-//! 
-//! On EFI, we don't use the generic memory manage code. This is because,
-//! while we're still in boot service mode, the firmware is in control of
-//! the memory map and we should use memory allocation services to allocate
-//! memory. Since it is possible for the memory map to change underneath us,
-//! we cannot just get the memory map once during init and use it with the 
-//! generic MM code.
+//!
+//! On EFI, we don't use the generic memory manage code. This is because, while we're still in boot service mode, the
+//! firmware is in control of the memory map and we should use memory allocation services to allocate memory. Since it
+//! is possible for the memory map to change underneath us, we cannot just get the memory map once during init and use
+//! it with the generic MM code.
 
 use uefi::table::boot::{BootServices, MemoryType};
 
@@ -16,8 +14,7 @@ use uefi::ResultExt;
 const EFI_PAGE_SIZE: u64 = 0x1000;
 
 #[derive(Debug)]
-pub struct MemoryManager {
-}
+pub struct MemoryManager {}
 
 impl MemoryManager {
     pub fn new() -> Self {
@@ -42,7 +39,7 @@ impl MemoryManager {
         assert!(desc_iter.len() > 0, "Memory map is empty");
 
         // For information purposes, we print out a list of all the usable memory
-        // we see in the memory map. Don't print out everything, the memory map 
+        // we see in the memory map. Don't print out everything, the memory map
         // is probably pretty big (e.g. OVMF under QEMU returns a map with nearly
         // 50 entries here).
         info!("efi: usable memory ranges ({} total)", desc_iter.len());
@@ -51,9 +48,12 @@ impl MemoryManager {
                 MemoryType::CONVENTIONAL => {
                     let size = descriptor.page_count * EFI_PAGE_SIZE;
                     let end_address = descriptor.phys_start + size;
-                    info!("> {:#x} - {:#x} ({} KiB)", descriptor.phys_start, end_address, size);
-                },
-                _ => {},
+                    info!(
+                        "> {:#x} - {:#x} ({} KiB)",
+                        descriptor.phys_start, end_address, size
+                    );
+                }
+                _ => {}
             }
         }
     }
